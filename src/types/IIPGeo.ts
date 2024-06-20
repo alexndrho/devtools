8cf5b439e6d273ebd5dd4028d8432f01ed8523c6
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2024-06-20 11:58:53 +0800

    fix(type): make ip geo fields optional

--- src/types/IIPGeo.ts
+++ src/types/IIPGeo.ts
@@ -2,19 +2,19 @@ interface IIPGeo {
   status: 'success' | 'fail';
   // Included only if status is 'fail'
   message?: 'private range' | 'reserved range' | 'invalid query';
-  country: string;
-  regionName: string;
-  city: string;
-  zip: string;
-  lat: number;
-  lon: number;
-  timezone: string;
-  isp: string;
-  org: string;
-  mobile: boolean;
-  proxy: boolean;
-  hosting: boolean;
-  query: string;
+  country?: string;
+  regionName?: string;
+  city?: string;
+  zip?: string;
+  lat?: number;
+  lon?: number;
+  timezone?: string;
+  isp?: string;
+  org?: string;
+  mobile?: boolean;
+  proxy?: boolean;
+  hosting?: boolean;
+  query?: string;
 }
 
 export default IIPGeo;
